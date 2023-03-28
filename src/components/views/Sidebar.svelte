fa00c15842a12bb509cd84eacb9e806018e1ed1b
mili <kamil.maksudov2019@gmail.com>
2023-03-29 00:32:03 +0200

    depl: add nginx, update docker + cloudbuild

--- src/components/views/Sidebar.svelte
+++ src/components/views/Sidebar.svelte
@@ -10,7 +10,9 @@
 	}
 </script>
 
-<aside class="hidden flex-col items-center justify-between rounded-md bg-white p-10 lg:flex">
+<aside
+	class="hidden h-screen flex-col items-center justify-between rounded-md bg-white p-10 lg:sticky lg:top-0 lg:flex"
+>
 	<a href="/">
 		<h1 class="text-3xl font-black">lines</h1>
 	</a>
@@ -18,28 +20,28 @@
 	<nav class="flex flex-1 flex-col justify-center gap-4 text-lg">
 		<a
 			href="/"
-			class="flex	items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
+			class="flex items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
 		>
 			<Icon src={icons.home} size="24" title={sidebar.en.nav.home.alt} />
 			<span>{sidebar.en.nav.home.name}</span>
 		</a>
 		<a
 			href={loggedIn ? '/library' : '/'}
-			class="flex	items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
+			class="flex items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
 		>
 			<Icon src={icons.library} size="24" title={sidebar.en.nav.library.alt} />
 			<span>{sidebar.en.nav.library.name}</span>
 		</a>
 		<a
 			href="/"
-			class="flex	items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
+			class="flex items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
 		>
 			<Icon src={icons.playlist} size="24" title={sidebar.en.nav.playlist.alt} />
 			<span>{sidebar.en.nav.playlist.name}</span>
 		</a>
 		<a
 			href="/"
-			class="flex	items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
+			class="flex items-center gap-4 text-xl transition-colors hover:fill-neutral-500 hover:text-neutral-500"
 		>
 			<Icon src={icons.trending} size="24" title={sidebar.en.nav.trending.alt} />
 			<span>{sidebar.en.nav.trending.name}</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,13 +23,13 @@
 	<meta name="description" content="Video Streaming Platform" />
 </svelte:head>
 
-<div class="flex h-full gap-5">
+<div class="flex h-full gap-5 p-5">
 	<Sidebar />
 	<main class="flex h-full w-full flex-col gap-5">
 		<Header />
-		<section class="flex h-fit flex-col  gap-5 rounded-md bg-white p-10	">
+		<section class="flex h-fit flex-col gap-5 rounded-md bg-white p-10">
 			<div class="flex justify-between">
-				<h1 class=" text-5xl font-bold ">POV</h1>
+				<h1 class=" text-5xl font-bold">POV</h1>
 				<PaginationBtn />
 			</div>
 			<div class="flex flex-wrap gap-5">
